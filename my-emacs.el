e8eb092ba2de09f5bb4c19d33a38892758af8446
jeongyoung <jeongyoung@gmail.com>
2012-11-14 12:11:20 +0900

    script

--- my-emacs.el
+++ my-emacs.el
@@ -7,15 +7,15 @@
 (let ((type (emacs-type)))
   ;;mac
   (cond ((eq type 'emacs-mac-window)
-	 (set-face-font 'default "NanumGothicCoding-14") 
+	
 	 ; mac에서 command 를 meta로 사용
 	 (setq ns-command-modifier 'meta))
 
 	)
   ;;windows
   (cond ((eq type 'emacs-window)
-	 (set-face-font 'default "NanumGothicCoding-14"))
 	 )
+	)
 )
 	 
 	
--- my-visual.el
+++ my-visual.el
@@ -4,11 +4,19 @@
 
 (if (functionp 'tool-bar-mode) (tool-bar-mode 0))
 
+;; set font
+;(if window-system
+;	(set-face-font 'default "-apple-NanumGothicCoding-medium-normal-normal-*-13-*-*-*-m-0-*-*"))
+
+
+(set-face-font 'default "NanumGothicCoding-14") 
+	
+;(setq default-frame-alist '((font . "NanumGothicCoding-14")))
 
 ; (set-cursor-color 'red)
 ; (set-cursor-color 'red)                                                       
 (setq default-frame-alist                                                       
-          ' ((cursor-color . "red") (cursor-type . box) (width . 60) (height . 55)))  
+          ' ((cursor-color . "red") (cursor-type . box) (width . 60) (height . 55) ))  
 
 ;; 모드 라인에 Display time mode 활성화
 (display-time-mode t)
