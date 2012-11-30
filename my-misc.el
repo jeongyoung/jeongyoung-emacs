30ea57d5bec786ef7422e3136a471aa9640ed1c2
jeongyoung <jeongyoung@gmail.com>
2012-11-30 13:01:53 +0900

    trump ssh for AWS

--- my-misc.el
+++ my-misc.el
@@ -4,4 +4,10 @@
 ;(setq mac-command-modifier 'meta)
 
 
+(tramp-set-completion-function "ssh"
+                               '((tramp-parse-sconfig "/etc/ssh_config")
+                                 (tramp-parse-sconfig "~/.ssh/config")))
+
+
+
 
