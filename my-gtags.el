de81ade41319912628d1e1430712fdc4ff169bd2
jeongyoung <jeongyoung@gmail.com>
2013-12-05 08:13:52 +0900

    update

---
+++ my-gtags.el
@@ -0,0 +1,5 @@
+;(autoload 'ggtags-mode "gtags" "" t)
+(add-hook 'c-mode-common-hook
+					(lambda ()
+						(when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+							(ggtags-mode 1))))
---
+++ my-reload-dotemacs.el
@@ -0,0 +1,4 @@
+(defun reload-dotemacs ()
+  "Reload .emacs.d/init.el"
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
